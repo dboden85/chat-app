01a2ac5670f88659b2cfb5b5a1bfbd1936e049fc
David <davidboden44@gmail.com>
2023-10-05 09:49:47 -0400

    unignored package.json

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,11 +1,35 @@
 import Bubbles from './Bubbles';
 import classes from './Conversation.module.css';
 
-const Conversation = (props) => {
+const Conversation = () => {
+
+  const DUMMYCONVO = [
+    {
+    id : 'c1',
+    name : 'Mark',
+    message : 'Hi Dave!',
+    isUser : false
+  },
+  {
+    id : 'c2',
+    name : 'Dave',
+    message : 'Hi Mark!',
+    isUser : true
+  }
+]
+
   return (
     <div className={classes['convo-box']}>
       <div className={classes.convos}>
-        <Bubbles/>
+        <ul>
+        {
+          DUMMYCONVO.map(chat=>{
+            return(
+            <Bubbles id={chat.id} isUser={chat.isUser} mess={chat.message}/>
+            )
+          })
+        }
+        </ul>
       </div>
     </div>
   )
