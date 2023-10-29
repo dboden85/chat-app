01677d1e9e792ce731884f8dea243fd6437f9f2d
David <davidboden44@gmail.com>
2023-10-29 16:20:40 -0400

    added hover interaction and signaling when someone is online

--- client/src/components/friendslist/FriendsList.jsx
+++ client/src/components/friendslist/FriendsList.jsx
@@ -7,25 +7,31 @@ const FriendsList = (props)=>{
             id: 1,
             name: 'Mark Noesen',
             userid: 1,
+            online: true
         },
         {
             id: 2,
             name: 'Talan Ferugson-Boden',
             userid: 2,
+            online: false
         },
         {
             id: 3,
             name: 'Derek Grantham',
             userid: 3,
+            online: true
         },
         {
             id: 4,
             name: 'Dave Poopyface',
             userid: 4,
+            online: false
         }
     ])
 
-    console.log(friends);
+    const onClass = classes.friend + ' ' + classes.online;
+    const offClass = classes.friend;
+
     return(
         <div className={classes.friendslist}>
             <div className={classes.friendshead}>
@@ -35,7 +41,7 @@ const FriendsList = (props)=>{
                 <ul>
                 {
                     friends.map(friend =>(
-                        <li key={friend.id} className={classes.friend}>{friend.name}</li>
+                        <li key={friend.id} className={friend.online ? onClass : offClass}>{friend.name}</li>
                     ))
                 }
                 </ul>
